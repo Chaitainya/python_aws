5eb3eca14c8eaa52912de9af330d7c84a1005b03
Chaitainya <76576779+Chaitainya@users.noreply.github.com>
2022-08-14 17:38:53 +0530

    startingInstanceFix_withLambdaFix_stoppingWithLambda

--- 05_startingAnInstanceUsingLambda.py
+++ 05_startingAnInstanceUsingLambda.py
@@ -1,8 +1,8 @@
 import boto3
-region = 'ap-south-1b'
+region = 'ap-south-1'
 instances = ['i-06441158abf760eaf', 'i-081343f82001a599a']
 ec2 = boto3.client('ec2', region_name=region)
 
 def lambda_handler(event, context):
-    ec2.stop_instances(InstanceIds=instances)
-    print('stopped your instances: ' + str(instances))
+    ec2.start_instances(InstanceIds=instances)
+    print('started your instances: ' + str(instances))
