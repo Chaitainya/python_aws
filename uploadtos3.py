e8fba9074cb79916359df5aa8ebbbcab4aaffe71
Chaitainya <76576779+Chaitainya@users.noreply.github.com>
2022-10-21 13:36:25 +0530

    Update uploadtos3.py

--- uploadtos3.py
+++ uploadtos3.py
@@ -4,5 +4,5 @@ import os
 userinput = win32api.MessageBox(0, 'Successfully uploaded to s3 bucket. Would you like to deploy?', 'S3 Upload status', 0x00000004)
 print(userinput)
 # os.system('dir')
-if userinput == 7:
-  win32api.MessageBox(0, 'Deployment started', 'Deploment status')
+if userinput == 6:
+    win32api.MessageBox(0, 'Deployment started', 'Deploment status')
