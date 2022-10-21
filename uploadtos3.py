53bfe17aa2ec41807c4f1845d43a5f858a5f7740
Chaitainya <76576779+Chaitainya@users.noreply.github.com>
2022-10-21 15:53:16 +0530

    Code updated

--- uploadtos3.py
+++ uploadtos3.py
@@ -23,3 +23,27 @@ if userinput == 6:
     if reason != None:
         os.system("ssh -l user -p port number ipaddress build jobname -s -v -p REASON='" + reason + "'")
         win32api.MessageBox(0, 'Successfully deployed.', 'Deploment status')
+
+        
+#########################################################################
+
+# if any error like module not found install pip install pypiwin32 # if any error occurs while installing pypiwin32 run this command: python -m pip install --user --upgrade pip
+import win32api, os, pywin.mfc.dialog, sys
+
+environment = sys.argv[1]
+
+userinput = win32api.MessageBox(0, 'Successfully uploaded to s3 bucket. Would you like to deploy?', 'S3 Upload status', 0x00000004)
+print(userinput)
+
+if userinput == 6:
+    reason = pywin.mfc.dialog.GetSimpleInput("Why are you deploying?")
+    if reason != None:
+        if environment == "QA":
+            os.system("ssh -l user -p port number ipaddress build jobname" + environment + "' -s -v -p REASON='" + reason + "'")
+        elif environment == "STAGE":
+            os.system("ssh -l user -p port number ipaddress build jobname" + environment + "' -s -v -p REASON='" + reason + "'")
+        elif environment == "PROD":
+            os.system("ssh -l user -p port number ipaddress build jobname" + environment + "' -s -v -p REASON='" + reason + "'")
+        win32api.MessageBox(0, 'Successfully deployed.', 'Deployment status')
+    else:
+        win32api.MessageBox(0, 'Without a valid reason you can never deploy.', 'Deploment status')
